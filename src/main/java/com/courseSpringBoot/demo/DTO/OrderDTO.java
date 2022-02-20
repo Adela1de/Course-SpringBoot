d0e20a46eb9742958dcfa24fa2c68b100ce455d8
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-02-20 20:13:55 -0300

    Enum OrderStatus Entity

--- src/main/java/com/courseSpringBoot/demo/DTO/OrderDTO.java
+++ src/main/java/com/courseSpringBoot/demo/DTO/OrderDTO.java
@@ -1,8 +1,10 @@
 package com.courseSpringBoot.demo.DTO;
 
 import com.courseSpringBoot.demo.entities.User;
+import com.courseSpringBoot.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
@@ -12,6 +14,7 @@ public class OrderDTO {
 
     private Long id;
     private Instant moment;
+    private OrderStatus orderStatus;
     @JsonIgnore
     private User client;
 }
--- src/main/java/com/courseSpringBoot/demo/config/TestConfig.java
+++ src/main/java/com/courseSpringBoot/demo/config/TestConfig.java
@@ -4,6 +4,7 @@ import com.courseSpringBoot.demo.entities.Category;
 import com.courseSpringBoot.demo.entities.Order;
 import com.courseSpringBoot.demo.entities.Product;
 import com.courseSpringBoot.demo.entities.User;
+import com.courseSpringBoot.demo.entities.enums.OrderStatus;
 import com.courseSpringBoot.demo.repositories.CategoryRepository;
 import com.courseSpringBoot.demo.repositories.OrderRepository;
 import com.courseSpringBoot.demo.repositories.ProductRepository;
@@ -67,9 +68,9 @@ public class TestConfig implements CommandLineRunner {
 
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELLED, u1);
 
         userService.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
