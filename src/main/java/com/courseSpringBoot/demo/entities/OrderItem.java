bafbd63fe0e53ac58e6bb8585efb29774161ecfa
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-02-20 23:11:16 -0300

    ManyToMany association between Order and Product with composite PK

---
+++ src/main/java/com/courseSpringBoot/demo/entities/OrderItem.java
@@ -0,0 +1,59 @@
+package com.courseSpringBoot.demo.entities;
+
+import com.courseSpringBoot.demo.entities.pk.OrderItemPK;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Exclude
+    private Integer quantity;
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Exclude
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price)
+    {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder()
+    {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order)
+    {
+        id.setOrder(order);
+    }
+
+    public Product getProduct()
+    {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product)
+    {
+        id.setProduct(product);
+    }
+
+}
---
+++ src/main/java/com/courseSpringBoot/demo/entities/pk/OrderItemPK.java
@@ -0,0 +1,29 @@
+package com.courseSpringBoot.demo.entities.pk;
+
+import com.courseSpringBoot.demo.entities.Order;
+import com.courseSpringBoot.demo.entities.Product;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+@EqualsAndHashCode
+public class OrderItemPK implements Serializable {
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Product product;
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Order order;
+
+}
